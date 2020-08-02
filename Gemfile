8f40543b92c2de0267b17ac04ca2b16d7108a536
2suke <takozawa3@gmail.com>
2020-08-03 05:33:08 +0900

    use acts-as-taggable-on

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.6.6'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
-
+gem 'acts-as-taggable-on', '~> 6.0'
 gem 'devise'
 gem 'haml-rails'
 # Use sqlite3 as the database for Active Record
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,8 @@
 class Board < ApplicationRecord
+  acts_as_taggable
+  acts_as_taggable_on :skills, :interests
   belongs_to :user
   has_many :comments
   validates :title, presence: true, length: { maximum: 20 }
+  validates :tag_list, length: { maximum: 10 }
 end
